2e3930844a5c00b08096e6b436643f4f94dc441d
cpuks <lukascpu@gmail.com>
2020-09-18 00:01:10 +0200

    Update __init__.py
    add wh25batt sensor support

--- custom_components/ecowitt/__init__.py
+++ custom_components/ecowitt/__init__.py
@@ -149,6 +149,7 @@ TYPE_WH40BATT = "wh40batt"
 TYPE_WH26BATT = "wh26batt"
 TYPE_WH65BATT = "wh65batt"
 TYPE_WH57BATT = "wh57batt"
+TYPE_WH25BATT = "wh25batt"
 TYPE_SOILBATT1 = "soilbatt1"
 TYPE_SOILBATT2 = "soilbatt2"
 TYPE_SOILBATT3 = "soilbatt3"
@@ -391,6 +392,8 @@ SENSOR_TYPES = {
     TYPE_WH65BATT: ("WH65 Battery", "BATT", TYPE_SENSOR,
                     None, "mdi:battery", 0),
     TYPE_WH57BATT: ("WH57 Battery", "BATT", TYPE_SENSOR,
+                    None, "mdi:battery", 0),
+    TYPE_WH25BATT: ("WH25 Battery", "BATT", TYPE_SENSOR,
                     None, "mdi:battery", 0),
     TYPE_SOILBATT1: ("Soil Moisture 1 Battery", "BATT", TYPE_SENSOR,
                      None, "mdi:battery", 0),
